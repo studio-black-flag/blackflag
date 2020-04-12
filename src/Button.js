db54000ffd9846520a9a05598153a66990215fec
Marcelo Kohl <marcelokohl@gmail.com>
2020-04-12 12:39:18 -0300

    test docz

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import t from 'prop-types'
+
+export const Button = props => {
+  return (
+    <div className='Button'>Button 2</div>
+  );
+};
+
+export default Button;
