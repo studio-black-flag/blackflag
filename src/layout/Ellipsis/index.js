904844cc622d0e8a76d0fcd598a14780905a797a
Marcelo Kohl <marcelokohl@gmail.com>
2020-07-13 18:26:52 -0300

    ellipsis update

--- src/layout/Ellipsis/index.js
+++ src/layout/Ellipsis/index.js
@@ -1,33 +1,33 @@
 import React, {useRef, useEffect, useState} from 'react'
 import {utils} from '../../'
 
-const Ellipsis = ({children, className, lines, hide, ...props}) => {
+const Ellipsis = ({children, className, lines, scale, tag, hide, ...props}) => {
   if (hide) return null
 
   const [max, setMax] = useState(100000)
   const ref = useRef()
   useEffect(() => {
-    console.log('ref: ',ref.current.clientHeight);
-    // setMax((ref.current.clientWidth/10)*lines)
-    var el = ref.current;
-    var divHeight = el.offsetHeight
-    var lineHeight = parseInt(el.style.lineHeight);
-    var lines = divHeight / lineHeight;
-    console.log(ref.current.style);
+
+    var fontWeight = window.getComputedStyle(ref.current).getPropertyValue('font-weight')/400
+    var divHeight = ref.current.offsetHeight
+    var lineHeight = parseInt(window.getComputedStyle(ref.current).getPropertyValue('line-height'), 10)
+    setMax((children.length*fontWeight)/((divHeight / lineHeight)/lines)*scale)
   }, [])
 
   const c = (
     'Ellipsis'+
     (className?' '+className:'')
   )
-
+  const Tag = tag
   return (
-    <p ref={ref} className={c} {...props}>{utils.textEllipsis(children, max)}</p>
+    <Tag ref={ref} className={c} {...props}>{utils.textEllipsis(children, max)}</Tag>
   );
 };
 
 Ellipsis. defaultProps = {
-  lines:2
+  lines:1,
+  tag:'p',
+  scale:1
 }
 
 export { Ellipsis };
