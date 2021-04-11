fa875ca95b832d6958439edca2fe5934357d155c
marcelokohl <marcelokohl@gmail.com>
2021-04-11 20:39:50 -0300

    Textarea value

--- src/controls/Field/Textarea.js
+++ src/controls/Field/Textarea.js
@@ -1,16 +1,16 @@
 import React, {useRef} from 'react'
 
-const FieldTextarea = React.forwardRef(({value, ...props}, ref) => {
+const FieldTextarea = React.forwardRef(({onChange, ...props}, ref) => {
   if (!ref) {
     ref = useRef()
   }
   const onLocalChange = e => {
-    props.onChange(e)
+    onChange(e)
     ref.current.style.height = 'auto'
     ref.current.style.height = ref.current.scrollHeight + 'px'
   }
   return (
-    <textarea {...props} onChange={onLocalChange} ref={ref}>{value}</textarea>
+    <textarea {...props} onChange={onLocalChange} ref={ref} />
   )
 })
 
